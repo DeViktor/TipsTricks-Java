2164b4a21a8b25b7c0d4ebd763ba1b1ec0fba051
DeViktor <contato.4years@gmail.com>
2022-01-04 12:43:27 -0400

    two methods for helloworld

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,5 +1,8 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        JOptionPane.showMessageDialog(null, "Hello, World! by JOptionPane");
+        System.out.println("Hello, World! by SystemOutPrint");
     }
 }
